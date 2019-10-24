f4456d1c49f25db089001650f204f8730864e1cd
felitastic <killerkralle@gmail.com>
2019-10-24 14:49:19 +0200

    Endless Runner (rough controls + collectibles)

--- Assets/_MonsterShop_Assets/Scripts/EndlessRunner/Collectible.cs
+++ Assets/_MonsterShop_Assets/Scripts/EndlessRunner/Collectible.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public Controls controls;
+    public int value = 1;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("player"))
+        if (other.CompareTag("Player"))
         {
-            Controls.gold += 1;
-            Destroy(this, 0.25f);
+            controls.UpdateCollectedCount(value);
+            Destroy(this.gameObject, 0.25f);
         }
     }
+
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    if (other.collider.CompareTag("Player"))
+    //    {
+    //        controls.UpdateCollectedCount(value);
+    //        Destroy(this, 0.25f);
+    //    }
+    //}
+
 }
--- Assets/_MonsterShop_Assets/Scripts/EndlessRunner/Controls.cs
+++ Assets/_MonsterShop_Assets/Scripts/EndlessRunner/Controls.cs
@@ -1,9 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controls : MonoBehaviour
 {
-    public static int gold;
-   
+    public int CollectedCount;
+    public Text CollectedText;
+    public Camera Cam;
+    public Rigidbody Monster;
+    public float verticalSpeed = 1f;
+    public float horizontalSpeed = 1f;
+
+    private float newPosX;
+
+    public void Start()
+    {
+        newPosX = Monster.transform.position.x;
+    }
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            newPosX = Cam.ScreenToWorldPoint(touch.position).x;
+        }
+
+        if (!Mathf.Approximately(Monster.transform.position.x, newPosX))
+        {
+            if (Monster.transform.position.x > newPosX)
+            {
+                //left
+                Monster.velocity = new Vector2(-horizontalSpeed, verticalSpeed);
+            }
+            else if (Monster.transform.position.x < newPosX)
+            {
+                //right
+                Monster.velocity = new Vector2(horizontalSpeed, verticalSpeed);
+            }
+        }
+        else
+        {
+            Monster.velocity = new Vector2(0f, verticalSpeed);
+        }
+
+        Cam.transform.position = new Vector3(0f, Monster.transform.position.y + 5.0f, -10f);
+    }
+
+    public void UpdateCollectedCount(int value)
+    {
+        CollectedCount += value;
+        CollectedText.text = ""+CollectedCount;
+    }
 }
