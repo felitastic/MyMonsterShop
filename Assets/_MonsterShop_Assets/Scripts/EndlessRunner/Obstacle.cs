21caca50188a402993a595335c9ec93d3578f58f
felitastic <killerkralle@gmail.com>
2019-11-04 17:29:09 +0100

    Temporary reload scene when game over in Endless Runner
    For testing purposes

--- Assets/_MonsterShop_Assets/Scripts/EndlessRunner/Obstacle.cs
+++ Assets/_MonsterShop_Assets/Scripts/EndlessRunner/Obstacle.cs
@@ -13,7 +13,16 @@ public class Obstacle : MonoBehaviour
         {
             GameOver.SetActive(true);
             other.gameObject.SetActive(false);
+            StartCoroutine(ReloadScene(other.gameObject));
         }
     }
 
+    public IEnumerator ReloadScene(GameObject monster)
+    {
+        yield return new WaitForSeconds(0.5f);
+        monster.transform.position = new Vector2(0f, 0f);
+        GameOver.SetActive(false);
+        yield return new WaitForSeconds(0.15f);
+        monster.SetActive(true);
+    }
 }
